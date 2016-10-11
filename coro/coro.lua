1a8af1c41eb94045d1801fb1146d72df097edee4
Ryan Hartlage <ryanplusplus@gmail.com>
2016-10-11 19:59:19 -0400

    Sequential vs. concurrent

--- coro/coro.lua
+++ coro/coro.lua
@@ -1,19 +1,40 @@
 local timer = require 'timer'
+local split = require 'coro-split'
 
-local coro = function(f)
+local function coro(f)
   coroutine.wrap(f)()
 end
 
+local function time(f)
+  local start = os.time()
+  f()
+  print('time taken: ' .. os.difftime(os.time(), start))
+end
+
 coro(function()
-  local x = (function()
-    timer.sleep(1000)
-    return 4
-  end)()
+  time(function()
+    local x = (function()
+      timer.sleep(1000)
+      return 4
+    end)()
 
-  local y = (function()
-    timer.sleep(1000)
-    return 5
-  end)()
+    local y = (function()
+      timer.sleep(1000)
+      return 5
+    end)()
 
-  print(x, y)
+    print(x, y)
+  end)
+
+  time(function()
+    local a, b = split(function()
+      timer.sleep(1000)
+      return 4
+    end, function()
+      timer.sleep(1000)
+      return 5
+    end)
+
+    print(a, b)
+  end)
 end)
