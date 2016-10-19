0961712b8755c2f9e1bcb3b7a7a2ef0c4e5341d7
Ryan Hartlage <ryanplusplus@gmail.com>
2016-10-19 08:19:43 -0400

    Check dependency mtime

--- ocean/ocean.lua
+++ ocean/ocean.lua
@@ -1,10 +1,4 @@
--- todo
--- ====
--- check dates on deps
--- error when no tree exists to build target
-
 local spawn = require 'coro-spawn'
-local split = require 'coro-split'
 local fs = require 'coro-fs'
 local pretty = require 'pretty-print'.prettyPrint
 
@@ -55,12 +49,33 @@ local function exists(target)
   return fs.stat(target) ~= nil
 end
 
+local function mtime(target)
+  local stat = fs.stat(target)
+  if stat then return stat.mtime end
+end
+
+local function is_before(mtime1, mtime2)
+  if mtime1.sec == mtime2.sec then
+    return mtime1.nsec < mtime2.nsec
+  else
+    return mtime1.sec < mtime2.sec
+  end
+end
+
+local tree_cache = {}
+
 local function generate_tree(target)
+  if tree_cache[target] then return tree_cache[target] end
+
+  local target_exists = exists(target)
+  local target_mtime = mtime(target)
+
   for _, rule in ipairs(rules) do
     local match = target:match(rule.target)
+    local out_of_date = false
 
     if match ~= nil then
-      local found_all_deps = true
+      local satisfied_all_deps = true
       local tree = {
         rule = rule,
         target = target,
@@ -71,16 +86,23 @@ local function generate_tree(target)
       for _, dep in ipairs(rule.deps) do
         local dep = dep:gsub('*', match)
         if not exists(dep) then
+          out_of_date = true
           local sub_tree = generate_tree(dep)
           if not sub_tree then
-            found_all_deps = false
+            satisfied_all_deps = false
             break
           end
           table.insert(tree.deps, sub_tree)
+        elseif not target_exists or is_before(target_mtime, mtime(dep)) then
+          out_of_date = true
         end
       end
 
-      if found_all_deps then
+      if satisfied_all_deps then
+        if target_exists and not out_of_date then
+          tree.complete = true
+        end
+        tree_cache[target] = tree
         return tree
       end
     end
@@ -89,38 +111,37 @@ end
 
 local function execute(tree)
   coro(function()
-    print('building: ' .. tree.target .. ' (' .. tree.match .. ')')
     tree.rule.builder(tree.target, tree.match)
-    print('finished: ' .. tree.target .. ' (' .. tree.match .. ')')
-    if tree.rule.subscribers[tree.target] then
-      split(table.unpack(tree.rule.subscribers[tree.target]))
+    tree.complete = true
+    for _, subscriber in ipairs(tree.rule.subscribers[tree.target] or {}) do
+      subscriber()
     end
   end)
 end
 
 local function build_tree(tree)
-  local target = tree.target
-
-  if exists(target) then return end
+  if tree.scheduled or tree.complete then return end
 
   tree.scheduled = true
 
-  local missing_deps = false
-  for _, dep in ipairs(tree.deps) do
-    if not exists(dep.target) then
-      missing_deps = true
-      dep.rule.subscribers[dep.target] = dep.rule.subscribers[dep.target] or {}
-      table.insert(dep.rule.subscribers[dep.target], function()
-        build_tree(tree)
-      end)
-      if not dep.scheduled then
-        build_tree(dep)
-      end
+  local todo_dep_count = 0
+  local function dep_completed()
+    todo_dep_count = todo_dep_count - 1
+    if todo_dep_count == 0 then
+      execute(tree)
     end
   end
 
-  if not missing_deps and not tree.started then
-    tree.started = true
+  for _, dep in ipairs(tree.deps) do
+    if not dep.complete then
+      todo_dep_count = todo_dep_count + 1
+      dep.rule.subscribers[dep.target] = dep.rule.subscribers[dep.target] or {}
+      table.insert(dep.rule.subscribers[dep.target], dep_completed)
+      build_tree(dep)
+    end
+  end
+
+  if todo_dep_count == 0 then
     execute(tree)
   end
 end
@@ -128,7 +149,18 @@ end
 local function run(target)
   coro(function()
     local tree = generate_tree(target)
+
+    if not tree then
+      print('error: no recipe for building target "' .. target .. '"')
+      return
+    end
+
+    if tree.complete then
+      print('nothing to be done for target "' .. target .. '"')
+    end
+
     -- pretty(tree)
+
     build_tree(tree)
   end)
 end
@@ -136,15 +168,23 @@ end
 --
 
 rule('build', {}, function()
+  print('Creating build directory...')
   fs.mkdirp('build')
 end)
 
 rule('build/*.o', { 'src/*.c', 'build' }, function(target, match)
+  print('Compiling ' .. target .. '...')
   exec('touch ' .. target)
 end)
 
-rule('build/a.o', { 'src/a.c', 'build' }, 'touch build/a.o')
+rule('build/a.o', { 'src/a.c', 'build' }, function(target)
+  print('Compiling ' .. target .. '...')
+  exec('touch build/a.o')
+end)
 
-rule('build/app.lib', { 'build/a.o', 'build/b.o', 'build/c.o', 'build' }, 'touch build/app.lib')
+rule('build/app.lib', { 'build/a.o', 'build/b.o', 'build/c.o', 'build' }, function(target)
+  print('Linking ' .. target .. '...')
+  exec('touch build/app.lib')
+end)
 
 run('build/app.lib')
--- ocean/package.lua
+++ ocean/package.lua
@@ -2,7 +2,6 @@ return {
   name = "ryanplusplus/lake",
   version = "0.0.1",
   dependencies = {
-    "creationix/coro-split@2.0.0",
     "creationix/coro-spawn@2.0.0",
     "creationix/coro-fs@2.2.1",
     "luvit/pretty-print@2.0.0"
