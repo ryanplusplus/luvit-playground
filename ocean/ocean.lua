d2bb62f625f486cd449e2f65314bb4fd8a89c681
Ryan Hartlage <ryanplusplus@gmail.com>
2016-10-19 06:16:31 -0400

    Build dependency tree

--- ocean/ocean.lua
+++ ocean/ocean.lua
@@ -1,9 +1,7 @@
 -- todo
 -- ====
 -- check dates on deps
--- alternatives
--- patterns
--- pass variables to recipe functions
+-- error when no tree exists to build target
 
 local spawn = require 'coro-spawn'
 local split = require 'coro-split'
@@ -15,6 +13,15 @@ local function coro(f)
 end
 
 local rules = {}
+
+local function exec(command)
+  local cmd, rest = command:match('(%S+)%s(.+)')
+  local args = {}
+  for arg in rest:gmatch('%S+') do
+    table.insert(args, arg)
+  end
+  spawn(cmd, { args = args }).waitExit()
+end
 
 local function rule(targets, deps, builder)
   if type(targets) == 'string' then
@@ -33,10 +40,9 @@ local function rule(targets, deps, builder)
       target = '^' .. target:gsub('*', '(%%S+)') .. '$',
       deps = deps,
       builder = builder,
-      subscribers = subscribers
+      subscribers = {}
     }
 
-    -- Prioritize non-pattern rules
     if target:match('*') then
       table.insert(rules, rule)
     else
@@ -49,14 +55,14 @@ local function exists(target)
   return fs.stat(target) ~= nil
 end
 
-local function get_tree(target)
+local function generate_tree(target)
   for _, rule in ipairs(rules) do
     local match = target:match(rule.target)
 
     if match ~= nil then
       local found_all_deps = true
       local tree = {
-        builder = rule.builder,
+        rule = rule,
         target = target,
         deps = {},
         match = match
@@ -65,7 +71,7 @@ local function get_tree(target)
       for _, dep in ipairs(rule.deps) do
         local dep = dep:gsub('*', match)
         if not exists(dep) then
-          local sub_tree = get_tree(dep)
+          local sub_tree = generate_tree(dep)
           if not sub_tree then
             found_all_deps = false
             break
@@ -81,14 +87,48 @@ local function get_tree(target)
   end
 end
 
-local function build_tree(tree)
+local function execute(tree)
+  coro(function()
+    print('building: ' .. tree.target .. ' (' .. tree.match .. ')')
+    tree.rule.builder(tree.target, tree.match)
+    print('finished: ' .. tree.target .. ' (' .. tree.match .. ')')
+    if tree.rule.subscribers[tree.target] then
+      split(table.unpack(tree.rule.subscribers[tree.target]))
+    end
+  end)
+end
 
+local function build_tree(tree)
+  local target = tree.target
+
+  if exists(target) then return end
+
+  tree.scheduled = true
+
+  local missing_deps = false
+  for _, dep in ipairs(tree.deps) do
+    if not exists(dep.target) then
+      missing_deps = true
+      dep.rule.subscribers[dep.target] = dep.rule.subscribers[dep.target] or {}
+      table.insert(dep.rule.subscribers[dep.target], function()
+        build_tree(tree)
+      end)
+      if not dep.scheduled then
+        build_tree(dep)
+      end
+    end
+  end
+
+  if not missing_deps and not tree.started then
+    tree.started = true
+    execute(tree)
+  end
 end
 
 local function run(target)
   coro(function()
-    local tree = get_tree(target)
-    pretty(tree)
+    local tree = generate_tree(target)
+    -- pretty(tree)
     build_tree(tree)
   end)
 end
